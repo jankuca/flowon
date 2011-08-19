b6d23cc83716a87fd146de56fb4e41e1f8e900a4
jankuca <jan@jankuca.com>
2011-08-19 17:08:09 +0200

    add "date:updated" field to Session

--- src/modules/models/session.js
+++ src/modules/models/session.js
@@ -1,2 +1,7 @@
-var Session = Model.define('session')
+var Session = Model.define('session', {
+	beforeSave: function () {
+		this.updateTimestamp('date:modified');
+		this.doc['date:modified']= this['date:modified'];
+	}
+});
 global.Session = Session;
